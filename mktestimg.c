8d13272ad6f2c98bf6b826343837fdae24af08f5
Collin Allen <collin@command-tab.com>
2013-06-05 15:28:37 -0700

    Work with 24c02 EEPROMs
    These are what I have on hand, so mktestimg will produce a 256 byte test blob

--- mktestimg.c
+++ mktestimg.c
@@ -1,12 +1,10 @@
-// create a 8192 byte binary file with 'rolling' 
-
 #include <stdio.h>
 
 int main(int argc, char **argv) {
     int i, j;
     static char *hex = "\00\x11\x22\x33\x44\x55\x66\x77\x88\x99\xaa\xbb\xcc\xdd\xee\xff";
 
-    for(j=0;j<8192/16;j++)
+    for(j=0;j<256/16;j++)
         for(i=0;i<16;i++)
             fputc(hex[j%16], stdout);
 }
