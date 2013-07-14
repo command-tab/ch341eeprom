9cdab6ed05e91b4604c67ed41bc640e2fcd879be
Collin Allen <collin@command-tab.com>
2013-07-14 13:19:47 -0700

    Clean up mktestimg formatting

--- mktestimg.c
+++ mktestimg.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 
-int main(int argc, char **argv) {
-    int i, j;
+int main(int argc, char **argv)
+{
     static char *hex = "\00\x11\x22\x33\x44\x55\x66\x77\x88\x99\xaa\xbb\xcc\xdd\xee\xff";
 
-    for(j=0;j<256/16;j++)
-        for(i=0;i<16;i++)
+    for (int j = 0; j < 256/16; j++)
+	{
+        for (int i = 0; i < 16; i++)
+		{
             fputc(hex[j%16], stdout);
+		}
+	}
+	
+	return 0;
 }
 
