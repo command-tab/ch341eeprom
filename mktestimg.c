51be7ee95e90419ffb1ba82562a913cec0d47e5d
Dmitry Frolov <dmitry.frolov@gmail.com>
2015-07-19 15:01:09 +0600

    Fix compilation with gcc

--- mktestimg.c
+++ mktestimg.c
@@ -2,11 +2,12 @@
 
 int main(int argc, char **argv)
 {
+    int i, j;
     static char *hex = "\00\x11\x22\x33\x44\x55\x66\x77\x88\x99\xaa\xbb\xcc\xdd\xee\xff";
 
-    for (int j = 0; j < 256/16; j++)
+    for (j = 0; j < 256/16; j++)
 	{
-        for (int i = 0; i < 16; i++)
+        for (i = 0; i < 16; i++)
 		{
             fputc(hex[j%16], stdout);
 		}
